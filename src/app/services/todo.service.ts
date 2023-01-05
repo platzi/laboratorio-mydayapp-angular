22c0bb3793c45e38fc0dc3680a62a3f505374e0d
Jaime Muñoz <jmunoz@agexport.org.gt>
2023-01-05 11:27:26 -0600

    using rxjs for emit changes

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { todoItem } from '../models/todoItem.interface';
-
+import { BehaviorSubject }  from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  private itemsSubject = new BehaviorSubject<todoItem[]>([]);
+  public todoItems$ = this.itemsSubject.asObservable();
   private todoItems: Array<todoItem> = []
   constructor() {
     const localStorageTodoItems = localStorage.getItem("mydayapp-angular")
@@ -14,9 +16,10 @@ export class TodoService {
    }
 
 
-  addTodoItem(todoitem:todoItem){
-    this.todoItems.push({...todoitem});
+  addTodoItem(newTodoItem:todoItem){
+    this.todoItems.push({...newTodoItem});
     localStorage.setItem("mydayapp-angular",JSON.stringify(this.todoItems))
+    this.itemsSubject.next(this.todoItems);
   }
 
   changeTodo(todoitem:todoItem){
@@ -25,10 +28,12 @@ export class TodoService {
       ...todoitem
     };
     localStorage.setItem("mydayapp-angular",JSON.stringify(this.todoItems))
+    this.itemsSubject.next(this.todoItems);
   }
 
   removeCompleteTodo(){
     this.todoItems = [ ...this.todoItems.filter(f=>f.completed!=true) ]
     localStorage.setItem("mydayapp-angular",JSON.stringify(this.todoItems))
+    this.itemsSubject.next(this.todoItems);
   }
 }
