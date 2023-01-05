b4fea132237753e6e67774ba965a5ac6cbfbe667
Jaime Muñoz <jmunoz@agexport.org.gt>
2023-01-05 09:33:00 -0600

    Add SharedModule and ComponentsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from './shared/shared.module'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule, 
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
