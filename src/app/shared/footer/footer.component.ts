86ef4946a8c21642ab2e30fd88062677fb9528fe
Jaime Muñoz <jmunoz@agexport.org.gt>
2023-01-06 18:52:36 -0600

    count todo Items, hidden html with id #footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,  OnDestroy } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
+import { map} from 'rxjs/operators'
+import { Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
+export class FooterComponent implements OnInit,  OnDestroy  {
+  totalItems$! :Subscription;
+  public countItems = 0
+  constructor(
+    private todoService: TodoService
+  ) { }
 
   ngOnInit(): void {
+     this.totalItems$ = this.todoService.todoItems$
+                                .pipe(
+                                  map(items => items.length )
+                                ).subscribe(
+                                  response => this.countItems = response
+                                )
   }
+
+  ngOnDestroy(): void {
+      this.totalItems$.unsubscribe()
+  }
+
+  
 
 }
