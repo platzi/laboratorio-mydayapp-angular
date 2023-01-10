33a6d2296c3afc463653e01c74e1dd842ed638ea
Jaime Muñoz <jmunoz@agexport.org.gt>
2023-01-09 18:56:54 -0600

    work todo Item

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input ,ElementRef } from '@angular/core';
 import { todoItem } from 'src/app/models/todoItem.interface';
 import { TodoService } from 'src/app/services/todo.service';
 @Component({
@@ -9,11 +9,27 @@ import { TodoService } from 'src/app/services/todo.service';
 export class TodoItemComponent implements OnInit {
   @Input() item!: todoItem;
   constructor(private todoService: TodoService) {}
+  public  edit = false;
+  ngOnInit(): void {
+    this.edit = false;
+  }
 
-  ngOnInit(): void {}
-
-  changeTodo() {
+  changeTodo(IsCheckCompleted:boolean) {
+    this.edit = false;
+    this.item.title = this.item.title.trim()
+    
+    if(this.item.title.length ===0) return 
+    if(IsCheckCompleted)
     this.item.completed = !this.item.completed;
+
     this.todoService.changeTodo(this.item);
+  }
+
+  editStatus(status:boolean, input:HTMLInputElement){
+    this.edit = status;
+  }
+
+  removeTodo(){
+    this.todoService.removeTodo(this.item);
   }
 }
