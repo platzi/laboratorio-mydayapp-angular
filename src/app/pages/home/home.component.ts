4f6184928a39ddf4b41c779fd228bc50a8188971
Franco <frcg2424@hotmail.com>
2023-01-12 00:38:50 -0500

    feat: :sparkles: Ocultar las secciones main y footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ITask } from 'src/app/models/task.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  tasks: ITask[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    var arrayTask = localStorage.getItem('tasks');
+    if (arrayTask) {
+      this.tasks = JSON.parse(arrayTask);
+    }
   }
-
 }
