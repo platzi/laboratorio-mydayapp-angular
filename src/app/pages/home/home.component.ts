3b092c33a94e054fec4d98514140f5a71a8006d6
Dark-Light-20 <pazsebas@hotmail.com>
2023-01-18 13:58:06 -0500

    Task 1: Hide main and footer when there are no tasks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,10 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
