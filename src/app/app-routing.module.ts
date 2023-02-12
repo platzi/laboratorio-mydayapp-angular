d1ff61feccce6ff803aadb8f672973d940f67658
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-02-12 15:40:10 -0500

    add routes for project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,15 @@ import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent
+  },
+  {
+    path: ':filter',
     component: HomeComponent,
-    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '',
   }
 ];
 
