e1725c039b9ce6ebf3fe821810e8b9ad80175ff6
miltondw <estradamilton2001@gmail.com>
2023-03-10 16:43:13 -0500

    service creation for persistence and methods for creating and deleting things todo

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { ITodo } from '@app/models/Todo.model'
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  private todoListSource = new BehaviorSubject<ITodo[]>([]);
+  key="mydayapp-angular"
+  constructor() {
+    const storedList = localStorage.getItem(this.key);
+    if (storedList) {
+      this.todoListSource.next(JSON.parse(storedList));
+    }
+  }
+
+  todoList$ = this.todoListSource.asObservable();
+
+  create(title: string) {
+    const currentList = this.todoListSource.getValue();
+    const todo: ITodo = {
+      title,
+      completed: false
+    };
+    currentList.push(todo);
+    this.todoListSource.next(currentList);
+    localStorage.setItem(this.key,JSON.stringify(currentList))
+  }
+
+  delete(id: number) {
+    const currentList = this.todoListSource.getValue();
+    currentList.splice(id, 1);
+    this.todoListSource.next(currentList);
+    localStorage.setItem(this.key,JSON.stringify(currentList))
+  }
+
 }
