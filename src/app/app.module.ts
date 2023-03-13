a3e1ae6f4819cf49658c7ece2703439dc1750ddb
JoniMeichtry11 <joni.meichtry@gmail.com>
2023-03-13 07:15:29 -0300

    feat: application divided into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InputGenericComponent } from './shared/input-generic/input-generic.component';
+import { InputCheckboxComponent } from './components/input-checkbox/input-checkbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    InputGenericComponent,
+    InputCheckboxComponent
   ],
   imports: [
     BrowserModule,
