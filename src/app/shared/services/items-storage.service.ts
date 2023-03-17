dcadb04a9335e7fcc6a5cd155b85ae9465eef3e0
Nicolas Erazo <nico.lacho@outlook.com>
2023-03-17 12:41:12 -0500

    Tests passed successfully

---
+++ src/app/shared/services/items-storage.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Item } from '../items.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsStorageService {
+
+  private storageKey = 'mydayapp-angular';
+
+  constructor() {}
+
+  get(): Item[] {
+    const items = localStorage.getItem(this.storageKey);
+    return items ? JSON.parse(items) : [];
+  }
+
+  update(items: Item[]): void {
+    localStorage.setItem(this.storageKey, JSON.stringify(items));
+  }
+}
