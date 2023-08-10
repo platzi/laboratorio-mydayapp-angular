e008f5d37abe7b4d1997daaf409aa8a67e8a1ed9
Julio Santaman <juliocsantaman@gmail.com>
2023-08-09 20:16:51 -0600

    Doing funcionalities from one to four.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms'; // Importa ReactiveFormsModule
+
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 
@@ -12,7 +15,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
@@ -6,9 +9,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[] = [];
+  taskForm!: FormGroup;
+  isEditTask: boolean = false;
+  taskToEdit!: number;
+  @ViewChild('editInput') editInput!: ElementRef;
+
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.taskForm = this.formBuilder.group({
+      task: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      //console.log(this.taskForm.value);
+    }
+  }
+
+  saveNewTask(event: any): void {
+    const inputValue = (event.target as HTMLInputElement).value.trim();
+    // console.log('Event', event);
+    // console.log('Value', inputValue);
+
+    if (inputValue != '') {
+      const idNumber: number = this.tasks.length > 0 ? parseInt(this.tasks[this.tasks.length - 1].id) + 1 : 1;
+      const id: string = idNumber.toString();
+
+      const task: Task = {
+        id: id,
+        title: inputValue,
+        completed: false
+      };
+
+      this.tasks.push(task);
+      //console.log('Task', task);
+
+      this.clearInput(this.taskForm.get('task'));
+    } else {
+      alert('The task is empty, type your task, please');
+    }
+  }
+
+  clearInput(abstractControl: AbstractControl | null): void {
+    if (abstractControl != null) {
+      abstractControl.setValue('');
+    }
+  }
+
+  changeTaskState(event: any, task: Task): void {
+    //console.log('event', event);
+    //console.log('Task', task.completed);
+    task.completed = !task.completed;
+    //console.log('Change task state', task.completed);
+  }
+
+  changeToEdit(editInputIndex: number): void {
+    this.isEditTask = true;
+    this.taskToEdit = editInputIndex;
+
+    setTimeout(() => {
+      if (this.editInput) {
+        console.log('this.editInput', this.editInput);
+        this.editInput.nativeElement.focus();
+      }
+    });
+  }
+
+  saveEdit(event: any, editInputIndex: number): void {
+    const inputValue = (event.target as HTMLInputElement).value.trim();
+    this.tasks[editInputIndex].title = inputValue;
+    this.isEditTask = false;
+
+  }
+
+  cancelEdit(): void {
+    this.isEditTask = false;
   }
 
 }
