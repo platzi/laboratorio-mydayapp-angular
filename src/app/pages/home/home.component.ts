e70b18a5b75e25de73ac64911dddc6f845ebb969
Jonathan Garcia <garciajonathan268@gmail.com>
2023-09-15 10:59:49 -0500

    🚧 todo funcionando

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+    selector: 'app-home',
+    templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    taskInput!: FormGroup
+    listAlone: boolean = true
 
-  ngOnInit(): void {
-  }
+    constructor(private builder: FormBuilder, private task:TaskService) {
+        this.taskInput = builder.group({
+            task: ['',Validators.required]
+        })
+    }
+
+    ngOnInit() {
+        const local = window.localStorage.getItem('mydayapp-angular')
+        if (local) {
+            const localParced = JSON.parse(local)
+            this.task.localTasks = localParced!
+        }
+        this.task.listAlone.subscribe({
+            next: (element:boolean) => {
+                this.listAlone = element
+            }
+        })
+    }
+
+    onSubmit(){
+        const sendElement = this.taskInput.get('task')?.value
+        if (sendElement) this.task.newTask = sendElement.trim()
+    }
+
+    onSend(e:KeyboardEvent){
+        if (e.key === 'Enter'){
+            this.onSubmit()
+            this.taskInput.reset()
+        }
+    }
 
 }
