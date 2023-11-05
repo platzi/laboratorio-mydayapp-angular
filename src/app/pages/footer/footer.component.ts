e80ee6cf286403e4c426bcba301dd097734c55a2
Carlos Gómez <drcgomez@hotmail.com>
2023-11-04 19:26:13 -0500

    Add item count and word to footer component.

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
+
+  @Input() countItems: number = 0;
+
+  getWord(): string {
+    return this.countItems === 1 ? 'item' : 'items';
+  }
 
 }
